5da90118a5cb603cb8e650219dc45eb91b72a342
EC2 Default User <ec2-user@ip-172-31-32-56.us-east-2.compute.internal>
2019-03-19 06:13:27 +0000

    stocklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
+  root 'welcome#index'
+  resources :user_stocks, except: [:show, :edit, :update]
+  
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: "stocks#search"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
